88f7ae2d627e13d0f118757b64cd45a292af9f19
Paul Crowder <paul.crowder@blackbaud.com>
2019-01-23 12:23:36 -0500

    Webpack tests

---
+++ spec/lib/webpack.spec.js
@@ -0,0 +1,54 @@
+const mock = require('mock-require');
+
+describe('Webpack', () => {
+  let webpack;
+  let fsExtraMock;
+  let findInFilesMock;
+
+  beforeEach(() => {
+    fsExtraMock = {
+      readFile: jasmine.createSpy('readFile'),
+      writeFile: jasmine.createSpy('writeFile')
+    };
+
+    findInFilesMock = {
+      find: jasmine.createSpy('find')
+    };
+
+    mock('fs-extra', fsExtraMock);
+    mock('find-in-files', findInFilesMock);
+
+    webpack = mock.reRequire('../../lib/webpack');
+  });
+
+  afterEach(() => {
+    mock.stop('fs-extra');
+  });
+
+  it('should remove json-loader prefixes from require statements', async () => {
+    fsExtraMock.readFile.and.returnValue(
+      `
+require('json-loader!../../somefile.json');
+`
+    );
+
+    findInFilesMock.find.and.returnValue({
+      'file1.ts': {
+        matches: [
+          'json-loader!'
+        ]
+      }
+    });
+
+    await webpack.fixLoaders();
+
+    expect(fsExtraMock.writeFile).toHaveBeenCalledWith(
+      'file1.ts',
+      `
+require('../../somefile.json');
+`
+    );
+
+  });
+
+});
